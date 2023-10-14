ba5fc458b8a6ce4d92bb6e1b7198d51559f19740
AdoDeveloper <105835711+AdoDeveloper@users.noreply.github.com>
2023-10-14 08:29:49 -0600

    cambio Models

--- SistemaWeb/src/java/Models/ViewModelEmpleados.java
+++ SistemaWeb/src/java/Models/ViewModelEmpleados.java
@@ -4,10 +4,22 @@
  */
 package Models;
 
+import java.util.Date;
+
 /**
  *
  * @author ernes
  */
 public class ViewModelEmpleados {
-    
+    //Atributos
+    private int ID_Empleado;
+    private String DUI_Empleado;
+    private int ISSS_Empleado;
+    private String NombresEmpleado;
+    private String ApellidosEmpleados;
+    private Date FechaNacEmpleado;
+    private String Telefono;
+    private String Correo;
+    private int ID_Cargo;
+    private int ID_Direccion;
 }
